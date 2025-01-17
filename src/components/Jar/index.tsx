90a005a1223eecc9a289c65294e18e63b12f093c
Lauren Ramgattie <lauren@withdouble.com>
2025-01-17 07:25:15 -0500

    ✨ Rename Jar component file and implement styled container for layout

--- src/components/Jar/index.tsx
+++ src/components/Jar/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Glowbug from './Glowbug'
-import { Glowbug as GlowbugType } from './Glowbug/types'
+import Glowbug from '../Glowbug'
+import { Glowbug as GlowbugType } from '../Glowbug/types'
+import { Outer } from './styled'
 
 type Props = {
   bugs: GlowbugType[]
@@ -9,11 +10,11 @@ type Props = {
 
 const Jar = ({ bugs, removeBug }: Props) => {
   return (
-    <div className="glowbug-jar">
+    <Outer>
       {bugs.map((bug) => (
         <Glowbug key={bug.id} bug={bug} onClick={() => removeBug(bug.id)} />
       ))}
-    </div>
+    </Outer>
   )
 }
 
---
+++ src/components/Jar/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Outer = styled.div`
+  position: relative;
+  width: 400px; // Adjust size of the jar
+  height: 600px; // Adjust height of the jar
+  border: 2px solid #0a9396;
+  border-radius: 20%; // Makes it look like a jar
+  overflow: hidden; // Ensures fireflies don't escape
+  background-color: #94d2bd;
+  opacity: 0.8;
+`
