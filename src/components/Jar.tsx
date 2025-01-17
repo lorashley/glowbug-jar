f7506e2a8191fd1fc072eee5ad56ea809e3957c5
lorashley <lor.ashley@gmail.com>
2025-01-17 06:49:23 -0500

    ✨base glowbug app

---
+++ src/components/Jar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Glowbug from "./Glowbug";
+import { Glowbug as GlowbugType } from "./Glowbug/types";
+
+type Props = {
+    bugs: GlowbugType[]
+    removeBug: (id: string) => void
+}
+
+const Jar = ({ bugs, removeBug}:Props) =>  {
+    return (
+      <div className="glowbug-jar">
+        {bugs.map((bug) => (
+            <Glowbug key={bug.id} bug={bug} onClick={() => removeBug(bug.id)}/>
+        ))}
+      </div>
+    );
+  }
+  
+  export default Jar;
+  
