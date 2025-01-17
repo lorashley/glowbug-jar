7038a811ac028712aa7a88a7aea51dac6c867434
Lauren Ramgattie <lauren@withdouble.com>
2025-01-17 07:12:56 -0500

    ✨ Integrate Chakra UI components and update configuration

--- src/components/JarInput.tsx
+++ src/components/JarInput.tsx
@@ -2,23 +2,28 @@ import { useCallback } from 'react'
 import { Glowbug, GlowbugKind } from './Glowbug/types'
 import { v4 as uuidv4 } from 'uuid'
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import { HStack } from '@chakra-ui/react'
 
 type Props = {
   addBug: (bug: Glowbug) => void
 }
 function JarInput({ addBug }: Props) {
-  const onButtonClick = useCallback((e: React.MouseEvent) => {
-    e.preventDefault()
-    addBug({
-      id: uuidv4(),
-      kind: GlowbugKind.PEE,
-    })
-  }, [])
+  const onButtonClick = useCallback(
+    (e: React.MouseEvent) => {
+      e.preventDefault()
+      addBug({
+        id: uuidv4(),
+        kind: GlowbugKind.PEE,
+      })
+    },
+    [addBug]
+  )
 
   return (
-    <form>
-      <button onClick={onButtonClick}>Add Bug</button>
-    </form>
+    <HStack>
+      <Button onClick={onButtonClick}>Add Bug</Button>
+    </HStack>
   )
 }
 
