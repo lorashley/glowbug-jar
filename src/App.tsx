e2b68cb2e1145ac04a5123c01df6848aab650540
Lauren Ramgattie <lauren@withdouble.com>
2025-01-17 20:48:58 -0500

    ✨ Integrate `useLocalStorage` hook for persistent bug state management in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import { useState, useCallback } from 'react'
+import { useCallback } from 'react'
 import Jar from './components/Jar'
 import AddBugButton from './components/AddBugButton'
 import { Glowbug } from './components/Glowbug/types'
-import React from 'react'
 import { ButtonsOuter, Outer } from './styled'
 import { H1 } from './system/Headings'
 import { theme } from './system/theme'
 import { Text } from '@chakra-ui/react'
 import { GlowbugKindArray } from './components/Glowbug/utils'
+import useLocalStorage from './hooks/useLocalStorage'
 
 function App() {
-  const [bugs, setBugs] = useState<Glowbug[]>([])
-  // TODO: hook up to work with local storage
-  //   () => {
-  //   const localStorageBugs = localStorage.getItem("glowbugs")
-  //   if(!localStorageBugs) return []
-  //   return JSON.parse(localStorageBugs) || [];
-  // }
-  // useEffect(() => {
-  //   localStorage.setItem("glowbugs", JSON.stringify(bugs));
-  // }, [bugs]);
+  const [bugs, setBugs] = useLocalStorage<Glowbug[]>('glowbugs', [])
 
-  const addBug = (bug: Glowbug) => setBugs((prevBugs) => [...prevBugs, bug])
-  const removeBug = useCallback((id: string) => {
-    setBugs((prevBugs) => prevBugs.filter((bug) => bug?.id !== id))
-  }, [])
+  const addBug = useCallback(
+    (bug: Glowbug) => {
+      setBugs([...bugs, bug])
+    },
+    [bugs, setBugs]
+  )
+
+  const removeBug = useCallback(
+    (id: string) => {
+      setBugs(bugs.filter((bug) => bug?.id !== id))
+    },
+    [bugs, setBugs]
+  )
 
   return (
     <Outer>
