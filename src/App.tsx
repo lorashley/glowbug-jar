f3b147228b7a84f7d66e2979c833f2f9b224afe7
Lauren Ramgattie <lauren@withdouble.com>
2025-01-17 21:21:51 -0500

    ✨ Add statistics display for bug count in App component and create Stat components for UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 import Jar from './components/Jar'
 import AddBugButton from './components/AddBugButton'
 import { Glowbug } from './components/Glowbug/types'
 import { ButtonsOuter, Outer } from './styled'
 import { H1 } from './system/Headings'
 import { theme } from './system/theme'
-import { Text } from '@chakra-ui/react'
 import { GlowbugKindArray } from './components/Glowbug/utils'
 import useLocalStorage from './hooks/useLocalStorage'
+import {
+  StatRoot,
+  StatLabel,
+  StatValueText,
+  StatValueUnit,
+} from '@/components/ui/stat'
 
 function App() {
   const [bugs, setBugs] = useLocalStorage<Glowbug[]>('glowbugs', [])
-
+  const goal = 20
   const addBug = useCallback(
     (bug: Glowbug) => {
       setBugs([...bugs, bug])
@@ -26,6 +31,8 @@ function App() {
     [bugs, setBugs]
   )
 
+  const bugsCount = bugs.length
+  const percentage = useMemo(() => (bugsCount / goal) * 100, [bugsCount, goal])
   return (
     <Outer>
       <H1 color={theme.colors.tertiary}>Glowbugs</H1>
@@ -37,9 +44,14 @@ function App() {
         ))}
       </ButtonsOuter>
 
-      <Text textStyle="md" color={theme.colors.lightText}>
-        Bug count: {bugs.length}
-      </Text>
+      <StatRoot color={theme.colors.tertiary}>
+        <StatLabel color={theme.colors.tertiary}>You've caught</StatLabel>
+
+        <StatValueText alignItems="baseline">
+          {bugsCount}{' '}
+          <StatValueUnit color={theme.colors.tertiary}>bugs</StatValueUnit>
+        </StatValueText>
+      </StatRoot>
     </Outer>
   )
 }
