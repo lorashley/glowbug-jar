cc617f2f03c11944d0446388a118158b95c52c24
Lauren Ramgattie <lauren@withdouble.com>
2025-01-19 14:21:35 -0500

    ✨ Enhance Settings component with dialog for user input and add NumberInput component

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,29 +1,56 @@
-import useSwitch from '@/hooks/useSwitch'
-import { PrimaryButton } from '@/system/Buttons'
-import { theme } from '@/system/theme'
-import { Box } from '@chakra-ui/react'
 import { IoSettingsSharp } from 'react-icons/io5'
+import { HStack } from '@chakra-ui/react'
+import {
+  DialogActionTrigger,
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { PrimaryButton } from '@/system/Buttons'
+import { Button } from '../ui/button'
+import { NumberInputField, NumberInputRoot } from '../ui/number-input'
+import { Field } from '../ui/field'
 
 const Settings = () => {
-  const [, , , toggleModal] = useSwitch()
+  const title = 'Settings'
   return (
-    <Box
-      flex={1}
-      flexDir={{ base: 'row' }}
-      display={{ base: 'flex' }}
-      gap={{ base: '2' }}
-      alignItems={{ base: 'center' }}
-      position={{ base: 'absolute' }}
-      top={{ base: '10' }}
-      right={{ base: '10' }}
-      color={theme.colors.lightText}
-      onClick={toggleModal}
-    >
-      <PrimaryButton onClick={toggleModal}>
-        <IoSettingsSharp />
-        Settings
-      </PrimaryButton>
-    </Box>
+    <HStack>
+      <DialogRoot size={'md'}>
+        <DialogTrigger asChild>
+          <PrimaryButton variant="outline">
+            <IoSettingsSharp />
+            {title}
+          </PrimaryButton>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+          </DialogHeader>
+          <DialogBody>
+            <Field
+              label="Enter Glowbug Goal"
+              helperText="Enter a number between 10 and 100"
+            >
+              <NumberInputRoot defaultValue="10" min={10} max={100}>
+                <NumberInputField />
+              </NumberInputRoot>
+            </Field>
+            <DialogFooter>
+              <DialogActionTrigger asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogActionTrigger>
+              <Button>Save</Button>
+            </DialogFooter>
+            <DialogCloseTrigger />
+          </DialogBody>
+        </DialogContent>
+      </DialogRoot>
+    </HStack>
   )
 }
 
